49556158c4eea5e31b4edbcfcf97a7344f066f03
Daniel Bueno Alves <daniel.alves@portoseguro.com.br>
2019-01-16 12:25:26 -0200

    Ajustes na home

--- daytrade/src/main/java/br/com/daytrade/controller/MainController.java
+++ daytrade/src/main/java/br/com/daytrade/controller/MainController.java
@@ -12,9 +12,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainController {
     
-    @GetMapping("/")
+    private LocalDate hoje = LocalDate.now();
+    
+    @GetMapping({"/", "/index"})
     public String index(Model model) {
         
+        model.addAttribute("tempoEstudo", this.calculaTempoEstudo());
         model.addAttribute("tempoHomeOffice", this.calculaTempoHomeOffice());
         model.addAttribute("terminoContrato", this.calculaTerminoContrato());
         
@@ -22,10 +25,18 @@ public class MainController {
     }
     
     
+    private String calculaTempoEstudo() {
+        LocalDate inicioEstudo = LocalDate.parse("2018-10-02");
+                
+        Period time = Period.between(inicioEstudo, hoje);
+                        
+        return "Tempo de estudo: " + time.getMonths() + " meses e " + time.getDays() + " dias.";
+
+    }
+    
     private String calculaTempoHomeOffice() {
         
         LocalDate inicioHomeOffice = LocalDate.parse("2018-06-01");
-        LocalDate hoje = LocalDate.now();
                 
         Period time = Period.between(inicioHomeOffice,hoje);
                 
